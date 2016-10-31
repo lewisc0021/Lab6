6100ac29c778dd59f98f89ec776133e62e52a7c4
Tommy Lewis <lewisc0021@gmail.com>
2016-10-31 15:14:52 -0400

    with validate input method

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -13,47 +13,68 @@ namespace Lab6
             Console.WriteLine("Welcome to the Pig Latin Generator.");
             while (true)
             {
-            Console.WriteLine("Please enter a word to convert into Pig Latin");
-            string word = Console.ReadLine();
-            //validateInput(word);
-            string pigword = TranslateToPigLatin(word);
-            Console.WriteLine(pigword);
-            Console.WriteLine("Do you wish to continue? (y/n)");
-            string answer = Console.ReadLine();
-            if (answer == "n" || answer == "N")
-                break;
+                Console.WriteLine("Please enter a word to convert into Pig Latin");
+                string word = Console.ReadLine();
+                if (validateInput(word))
+                {
+                    string pigword = TranslateToPigLatin(word);
+                    Console.WriteLine(pigword);
+                    Console.WriteLine("Do you wish to continue? (y/n)");
+                    string answer = Console.ReadLine();
+                    if (answer == "n" || answer == "N")
+                        break;
+                }
             }
         }
-       // static void validateInput(word)
-       // {
-        
+        static bool validateInput(string word)
+        {
+            char[] unexceptibleChars = { '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@', '#',
+            '$', '%', '^', '&', '*', '(', ')'};
+            
+            if (word.Trim() == "")
+            {                
+                Console.WriteLine("Please actually enter an input.");
+                return false;
+            }
+            else
+            {
+                foreach (char letter in word)
+                {
+                    if (unexceptibleChars.Contains(letter) == true)
+                    {
+                        Console.WriteLine("You've included some numbers and/or alphanumeric symbols. Please try again.");
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 
-        //}
         static string TranslateToPigLatin(string word)
         {
             //Variables used to evaluate first index
             string vowels = "aeiou";
             bool isVowel;
-            
+
             //Convert to lower case
             word = word.ToLower();
-            
+
             //Test for vowel
-            if (vowels.Contains(word[0])==true)
+            if (vowels.Contains(word[0]) == true)
             {
                 isVowel = true;
-            } 
+            }
             else
             {
-                isVowel = false;              
+                isVowel = false;
             }
-            
+
             //Adding 'way' to end
-            if (isVowel==true)
+            if (isVowel == true)
             {
                 word = word + "way";
             }
-            
+
             //Pre vowel consonsants moved to end / adding 'ay'
             else
             {
@@ -62,7 +83,7 @@ namespace Lab6
                 string word1 = word.Substring(0, vowelIndex);
                 string word2 = word.Substring(vowelIndex);
                 word = word2 + word1 + "ay";
-            }       
+            }
             return word;
         }
     }
